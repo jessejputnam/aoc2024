da486e5b95c606ef8e1ec94cb3183787d202c5db
Jesse Putnam <66268183+jessejputnam@users.noreply.github.com>
2024-12-08 14:51:01 -0500

    complete day04 part 2

--- day04.py
+++ day04.py
@@ -66,10 +66,51 @@ class CeresPart1:
                 self.check_diagonal(y, x, "down-left")
 
 
-data = get_input(4).text
+# data = get_input(4).text
 
+# matrix = data.split("\n")[:-1]
+
+# c1 = CeresPart1(matrix)
+# c1.analyze()
+# print(c1.get_valid_xmas_count())
+
+
+class CeresPart2:
+    def __init__(self, matrix: list[list[str]]):
+        self.data = matrix
+        self.valid_x_mas = 0
+
+    def add_count(self):
+        self.valid_x_mas += 1
+
+    def build_string(self, y: int, x: int, slash_dir: str) -> str:
+        if slash_dir == "back":
+            return self.data[y - 1][x - 1] + self.data[y][x] + self.data[y + 1][x + 1]
+        if slash_dir == "forward":
+            return self.data[y - 1][x + 1] + self.data[y][x] + self.data[y + 1][x - 1]
+
+    def check_x_mas(self, y: int, x: int):
+        if y < 1 or x < 1 or y > len(self.data) - 2 or x > len(self.data[0]) - 2:
+            return
+        back_slash = self.build_string(y, x, "back") in ["MAS", "SAM"]
+        forward_slash = self.build_string(y, x, "forward") in ["MAS", "SAM"]
+        if back_slash and forward_slash:
+            self.add_count()
+
+    def get_valid_x_mas_count(self) -> int:
+        return self.valid_x_mas
+
+    def analyze(self):
+        for y in range(len(self.data)):
+            for x in range(len(self.data[0])):
+                if self.data[y][x] != "A":
+                    continue
+                self.check_x_mas(y, x)
+
+
+data = get_input(4).text
 matrix = data.split("\n")[:-1]
 
-c = CeresPart1(matrix)
-c.analyze()
-print(c.get_valid_xmas_count())
+c2 = CeresPart2(matrix)
+c2.analyze()
+print(c2.get_valid_x_mas_count())
